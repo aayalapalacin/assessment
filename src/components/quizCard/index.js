d7e0e1ae40371ea54329a65462dd637639f68756
avokeys <alexander.ayala.palacin@gmail.com>
2022-11-16 20:43:08 +0000

    rendering message for perfect score, and failing

--- src/components/quizCard/index.js
+++ src/components/quizCard/index.js
@@ -4,221 +4,244 @@ import styles from "@styles/Home.module.css";
 import checkBoxStyle from "@styles/multiselect.module.css";
 import { useContext } from "react";
 import { types } from "@store/reducer";
-import Answer from "../Answer"
+import Answer from "../Answer";
 import Link from "next/link";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import { getThreshold } from "src/pages/quiz/[slug]";
 
 const QuizCard = () => {
   const [store, dispatch] = useContext(StoreContext);
-  const questions = store.questions
-  const currentQuestion = store.currentQuestion
-  const [threshold,setThreshold] = useState()
-  const [succesMessage,setSuccessMessage] = useState("")
-  const [successNext,setSuccessNext] = useState("")
-  const [failMessage,setFailMessage] = useState("")
-  const [failNext,setFailNext] = useState("")
-  const [finalScoreReady,setFinalScoreReady] = useState(0)
-  const router = useRouter()
+  const questions = store.questions;
+  const currentQuestion = store.currentQuestion;
+  const [threshold, setThreshold] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [successNext, setSuccessNext] = useState("");
+  const [failMessage, setFailMessage] = useState("");
+  const [failNext, setFailNext] = useState("");
+  const [finalScoreReady, setFinalScoreReady] = useState(0);
+  let thresholdItems= [];
+
+  const router = useRouter();
   const getRandom = (type) => {
     const index = Math.floor(Math.random() * store.templates[type].length);
     return store.templates[type][index];
-  }
-  
+  };
 
   // console.log("CURRENT_QUESTIONS", questions)
   const verifyAnswer = (score) => {
-    if(score === 1){
-
-      dispatch({ 
-        type: types.setGetCorrect,
-        payload: true
-      })
+    if (score === 1) {
       dispatch({
-        type: types.setScore
-      })
+        type: types.setGetCorrect,
+        payload: true,
+      });
+      dispatch({
+        type: types.setScore,
+      });
       setTimeout(() => {
-        dispatch({ 
+        dispatch({
           type: types.setGetCorrect,
-          payload: false
-        })
-      }, 1300)
-      return getRandom("correct")
+          payload: false,
+        });
+      }, 1300);
+      return getRandom("correct");
     } else {
-      return getRandom("incorrect")
+      return getRandom("incorrect");
     }
-  }
+  };
 
   const getResponse = (score) => {
     dispatch({
       type: types.setGetAnswer,
-      payload: true
-    })
+      payload: true,
+    });
     dispatch({
       type: types.setSelectedAnswer,
-      payload: verifyAnswer(score)
-    })
+      payload: verifyAnswer(score),
+    });
     setTimeout(() => {
-      dispatch({ 
+      dispatch({
         type: types.setGetAnswer,
-        payload: false
-      })
+        payload: false,
+      });
       dispatch({
         type: types.setMultiAnswerSelection,
-        payload: []
-      })
-      boxesArr.find(i => i.checked === true ? i.checked = false : null)
-    }, 1300)
-  }
+        payload: [],
+      });
+      boxesArr.find((i) => (i.checked === true ? (i.checked = false) : null));
+    }, 1300);
+  };
 
   let boxes = document.getElementsByName("isMultiselect");
-  let multiselection = []
+  let multiselection = [];
   var boxesArr = Array.prototype.slice.call(boxes, 0);
-  let checkedBoxes
+  let checkedBoxes;
 
   const verifyCurrentCheckbox = () => {
-      checkedBoxes = boxesArr.filter((checkbox) => {
-        return checkbox.checked;
-      });
+    checkedBoxes = boxesArr.filter((checkbox) => {
+      return checkbox.checked;
+    });
 
     multiselection = checkedBoxes.map((checkbox) => {
       return parseInt(checkbox.value);
-    })
+    });
     dispatch({
       type: types.setMultiAnswerSelection,
-      payload: multiselection
-    })
-  }
+      payload: multiselection,
+    });
+  };
 
   const selectAnswer = (score) => {
-    getResponse(score)
-    if(currentQuestion < questions.length - 1){
-      dispatch({ 
-        type: types.setCurrentQuestion
-      })
+    getResponse(score);
+    if (currentQuestion < questions.length - 1) {
+      dispatch({
+        type: types.setCurrentQuestion,
+      });
     } else {
-      clearInterval(store.timerRef)
+      clearInterval(store.timerRef);
       dispatch({
         type: types.setFinalScore,
-        payload: true
-      })
+        payload: true,
+      });
     }
-  }
+  };
 
   useEffect(() => {
-    if(questions.length <= 0){
-      clearInterval(store.timerRef)
+    if (questions.length <= 0) {
+      clearInterval(store.timerRef);
       dispatch({
         type: types.setFinalScore,
-        payload: true
-      })
-      
+        payload: true,
+      });
+
       setTimeout(() => {
-        router.push('/')
-      }, 5500)
+        router.push("/");
+      }, 5500);
     }
-  }, [questions])
-  
-  const thresholds = async()=>{ const response = await getThreshold(router.query.slug)
-  setThreshold(response.data);
-  }
+  }, [questions]);
 
-  useEffect(() =>{
-    if(store.showFinalScore){
-  thresholds();
-      setFinalScoreReady(store.score)
-    }
-  },[store.showFinalScore])
-console.log(finalScoreReady,"finalScoreReady State")
+  useEffect(async () => {
+    if (store.showFinalScore) {
+      console.log("test Thresh");
+      const response = await getThreshold(router.query.slug);
+      console.log("response.data", response.data);
 
-// if(threshold && finalScoreReady > 0){
+      setThreshold(response.data);
+      setFinalScoreReady(store.score);
+      if(response.data){
+        for(let i = 0; i < response.data.length; i++){
+            console.log(response.data[i].score_threshold,"score thresh")
+            console.log(store.score,"score store")
+            
+            if(store.score >= response.data[i].score_threshold ){
+                thresholdItems.push(response.data[i])    
+              }
+              else {
+                setFailMessage(response.data[response.data.length - 1].fail_message)
+                setFailNext([response.data.length - 1].fail_next)
+              }
+            }
+            
 
-//   for(let i = 0; i < threshold.length; i++){
+              setSuccessMessage(thresholdItems[0]?.success_message)
+              setSuccessNext(thresholdItems[0]?.success_next)
+            
+            console.log("thresholdItems", thresholdItems)
+          // response.data.forEach((e,index) =>{
+          //   thresholdObj[e.score_threshold]= e;
+          //   thresholdNum.push(e.score_threshold);
+          //   console.log(e.score_threshold,"score thresh")
+          //   console.log(store.score,"score store")
+          //   if(store.score >= 0 && store.score < ){}
+          //   if(store.score >= e.score_threshold){
+          //     setSuccessMessage(e.success_message)
+          //     setSuccessNext(e.success_next)
+          //   }
+          //   else if(store.score < e.score_threshold && index == response.data.length - 1){
+          //      setFailMessage(e.fail_message)
+          //      setFailNext(e.fail_next)
+          //   }
+          // })
+          
+          // setThreshold(thresholdObj);
+        }
 
-//       if(finalScoreReady >= threshold[i].score_threshold){
-//             setSuccessMessage(threshold[i].success_message)
-//             setSuccessNext(threshold[i].success_next)
-//           }
-//           else{
-//             // console.log("success")
-//              setFailMessage(threshold[threshold.length-1].fail_message)
-//              setFailNext(threshold[threshold.length-1].fail_next)
-//           }
+      }
+    }, [store.showFinalScore]);
+    console.log(finalScoreReady, "finalScoreReady State");
+    console.log(threshold, "threshold");
     
-//   }
-// }
-
-// threshold && threshold.forEach((e,index) =>{
-//   if(store.score >= e.score_threshold){
-//     setSuccessMessage(e.success_message)
-//     setSuccessNext(e.success_next)
-//   }
-//   else{
-//      setFailMessage(e.fail_message)
-//      setFailNext(e.fail_next)
-//   }
-// })
-console.log(succesMessage,"succesMessage")
-console.log(successNext,"successNext")
-console.log(failMessage,"failMessage")
-console.log(failNext,"failNext")
+    console.log(successMessage, "succesMessage");
+    console.log(successNext, "successNext");
+    console.log(failMessage, "failMessage");
+    console.log(failNext, "failNext");
+  
   const submitMultiselect = () => {
-    let verifyError = store.multiAnswerSelection.find(score => score === 0)
+    let verifyError = store.multiAnswerSelection.find((score) => score === 0);
 
-    if(verifyError === 0) {
+    if (verifyError === 0) {
       // console.log("INCORRECT", verifyError)
-      return selectAnswer(verifyError)
+      return selectAnswer(verifyError);
     } else {
       // console.log("CORRECT", 1)
-      return selectAnswer(1)
+      return selectAnswer(1);
     }
-  }
+  };
 
   return (
     <div className={styles.container}>
+      {store.getAnswer === true ? <Answer /> : null}
 
-    {store.getAnswer === true ? <Answer /> : null}
+      {store.showFinalScore === false && questions.length > 0 ? (
+        <>
+          <h1 className={styles.quiz_title_card}>
+            {questions[currentQuestion].title}
+          </h1>
 
-    {store.showFinalScore === false && questions.length > 0 ? (
-      <>
-        <h1 className={styles.quiz_title_card}>
-          {questions[currentQuestion].title}
-        </h1>
-
-        <div className={styles.quiz_grid}>
-          {Array.isArray(questions[currentQuestion].options) && questions[currentQuestion].options.map((option, i) => {
-            return (
-              <Fragment key={i}  >
-                {questions[currentQuestion].question_type === "SELECT" ? (
-                  <button
-                    key={option.id}
-                    name='isSelect'
-                    onClick={() => selectAnswer(option.score)}
-                    className={styles.quiz_card}>
-                    <h2 className={styles.buttonTextSelector}>
-                      {option.title}
-                    </h2>
-                  </button>
-                ) : questions[currentQuestion].question_type === "SELECT_MULTIPLE" ? (
-                  <>
-                    <label className={checkBoxStyle.multiSelect_label} key={option.id}>
-                      <input
-                        value={option.score}
-                        name='isMultiselect'
-                        type='checkbox'
-                        onChange={() => verifyCurrentCheckbox()}
-                        className={checkBoxStyle.buton_input}
-                      />
-                      <h2 className={checkBoxStyle.button_span} style={{ fontWeight: "normal" }}>
-                        {option.title}
-                      </h2>
-                    </label>
-                  </>
-                ) : <p>an error occurred, please report to your teacher</p>}
-              </Fragment>
-            )
-          })}
-        </div>
+          <div className={styles.quiz_grid}>
+            {Array.isArray(questions[currentQuestion].options) &&
+              questions[currentQuestion].options.map((option, i) => {
+                return (
+                  <Fragment key={i}>
+                    {questions[currentQuestion].question_type === "SELECT" ? (
+                      <button
+                        key={option.id}
+                        name="isSelect"
+                        onClick={() => selectAnswer(option.score)}
+                        className={styles.quiz_card}
+                      >
+                        <h2 className={styles.buttonTextSelector}>
+                          {option.title}
+                        </h2>
+                      </button>
+                    ) : questions[currentQuestion].question_type ===
+                      "SELECT_MULTIPLE" ? (
+                      <>
+                        <label
+                          className={checkBoxStyle.multiSelect_label}
+                          key={option.id}
+                        >
+                          <input
+                            value={option.score}
+                            name="isMultiselect"
+                            type="checkbox"
+                            onChange={() => verifyCurrentCheckbox()}
+                            className={checkBoxStyle.buton_input}
+                          />
+                          <h2
+                            className={checkBoxStyle.button_span}
+                            style={{ fontWeight: "normal" }}
+                          >
+                            {option.title}
+                          </h2>
+                        </label>
+                      </>
+                    ) : (
+                      <p>an error occurred, please report to your teacher</p>
+                    )}
+                  </Fragment>
+                );
+              })}
+          </div>
 
           {questions[currentQuestion].question_type === "SELECT_MULTIPLE" ? (
             <>
@@ -238,53 +261,81 @@ console.log(failNext,"failNext")
                   className={checkBoxStyle.multiSelect_SubmitButton}
                   style={{ textAlign: "center" }}
                 >
-                  <h2 style={{ fontWeight: "normal" }}>
-                    Send
-                  </h2>
+                  <h2 style={{ fontWeight: "normal" }}>Send</h2>
                 </button>
               )}
-
             </>
           ) : null}
-
-      </>
-    ) : (
-      <>
-      {/* <Link href={"/"} >
+        </>
+      ) : (
+        <>
+          {/* <Link href={"/"} >
        <a className={styles.backToHome}>
         Back to Home
       </a> 
       </Link> */}
 
-      {questions.length === 0 && (
-        <>
-          <span style={{fontSize: "var(--xxl)", margin: "10rem 10% 20px 10%", textAlign: "center"}}>
-            This quizz not have any questions to answer :c
-          </span>
-          <span style={{fontSize: "var(--m)", fontWeight: "200", margin: "20px 10%", textAlign: "center"}}>
-            redirecting to home...
-          </span>
+          {questions.length === 0 && (
+            <>
+              <span
+                style={{
+                  fontSize: "var(--xxl)",
+                  margin: "10rem 10% 20px 10%",
+                  textAlign: "center",
+                }}
+              >
+                This quizz not have any questions to answer :c
+              </span>
+              <span
+                style={{
+                  fontSize: "var(--m)",
+                  fontWeight: "200",
+                  margin: "20px 10%",
+                  textAlign: "center",
+                }}
+              >
+                redirecting to home...
+              </span>
+            </>
+          )}
+
+          {store.showFinalScore === true && questions.length > 0 && (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                margin: "10rem 0",
+              }}
+            >
+              <span
+                style={{
+                  fontSize: "var(--xxl)",
+                  textAlign: "center",
+                  margin: "20px 0",
+                }}
+              >
+                {Math.floor((store.score / questions.length) * 100)}% accuracy
+              </span>
+              <span style={{ fontSize: "var(--m)", margin: "20px 0" }}>
+                Your Score: {store.score} / {questions.length}
+                <br />
+              </span>
+              <span style={{ fontSize: "var(--m)", margin: "20px 0" }}>
+                Finished in: {store.timer} Seconds
+              </span>
+              <button
+                onClick={async () => {
+                  const response = await getThreshold(router.query.slug);
+                  console.log(response.data, "resp");
+                }}
+              >
+                threshold
+              </button>
+            </div>
+          )}
         </>
       )}
-
-      {store.showFinalScore === true && questions.length > 0 && (
-        <div style={{display: "flex", flexDirection: "column", alignItems: "center", margin: "10rem 0"}}>
-          <span style={{fontSize: "var(--xxl)", textAlign: "center", margin: "20px 0"}}>
-            {Math.floor(((store.score) / questions.length) * 100)}% accuracy
-          </span>
-          <span style={{fontSize: "var(--m)", margin: "20px 0"}}>
-            Your Score: {store.score} / {questions.length}<br/>
-          </span>
-          <span style={{fontSize: "var(--m)", margin: "20px 0"}}>
-            Finished in: {store.timer} Seconds
-          </span>
-          <button onClick={async()=>{ const response = await getThreshold(router.query.slug) 
-          console.log(response,"resp");
-          }}>threshold</button>
-        </div>
-      )}
-      </>
-    )}
     </div>
   );
 };
